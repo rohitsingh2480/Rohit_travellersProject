28236b3fde42dc0b3bbdb3a22226d3eb98a0b15f
rohitsingh2480 <64071423+rohitsingh2480@users.noreply.github.com>
2023-04-12 18:42:46 +0800

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,11 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "travellers_detail";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+	
+	//select_db('travellers_detail',$con);
+	session_start();
+?>
